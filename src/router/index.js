71779ff743cebe97d016a2ba6819e4edeb67e62b
ZYP <694534942@qq.com>
2018-08-20 11:40:41 +0800

    conflict fix

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,11 @@ Vue.use(Router)
 const view = (path, name) => () => import(`@/components/${path}${name}`)// 路由按需加载
 export default new Router({
   routes: [
-
+    // {
+    //   // path: '/',
+    //   // name: 'formom',
+    //   // component: view('', 'ForMyMomBirth')
+    // },
     {
       path: '/',
       name: 'Index',
