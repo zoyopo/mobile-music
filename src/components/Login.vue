cf6f117ecbe367e9b3d692891fd7ace34dee45d1
mr_zyp <694534942@qq.com>
2018-08-31 09:02:42 +0800

    login page skeleton

---
+++ src/components/Login.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="login">
+        <div class="logo"></div>
+        <div class="form-wrapper">
+            <div class="form">
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {},
+  watch: {}
+};
+</script>
+
+<style lang="scss" scoped>
+.login {
+  display: flex;
+  flex-direction: columns;
+  .logo {
+  }
+
+  .form-wrapper {
+    .form {
+    }
+  }
+}
+</style>
+
